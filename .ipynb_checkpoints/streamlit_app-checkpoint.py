7c8e9141241694e3f1785c48ed323999721fc31a
TRAORE <rasitraore9@gmail.com>
2024-11-08 18:08:10 +0000

    streamlit modification

--- .ipynb_checkpoints/streamlit_app-checkpoint.py
+++ .ipynb_checkpoints/streamlit_app-checkpoint.py
@@ -8,10 +8,7 @@ st.set_page_config(page_title="Application de gestion des prix immobiliers", lay
 @st.cache_data  # Met en cache les données pour éviter un rechargement à chaque changement de page
 def load_data():
     # Remplacez 'house_prices.csv' par le chemin de votre fichier de données
-    data = pd.read_csv("house_prices.csv")
+    data = pd.read_csv("data/train_cleaned.csv")
     return data
 
 data = load_data()
-
-# Navigation
-st.sidebar.title("Navigation")
