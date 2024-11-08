d4d3604fa442755fc18089c3c43540b3ebd4b487
TRAORE <rasitraore9@gmail.com>
2024-11-08 18:04:12 +0000

    modification de code

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,10 @@ import pandas as pd
 
 # Configuration de la page
 st.set_page_config(page_title="Application de gestion des prix immobiliers", layout="wide")
+
+# Chargement des données
+@st.cache_data  # Met en cache les données pour éviter un rechargement à chaque changement de page
+def load_data():
+    # Remplacez 'house_prices.csv' par le chemin de votre fichier de données
+    data = pd.read_csv("house_prices.csv")
+    return data
