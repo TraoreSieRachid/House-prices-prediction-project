941e70f4afc90b6744d4bd8d38cf65c2edcb902d
TRAORE <rasitraore9@gmail.com>
2024-11-11 18:18:29 +0000

    update  projet

--- streamlit_app.py
+++ streamlit_app.py
@@ -116,7 +116,7 @@ elif st.session_state.page == "Prédiction":
     if st.button("Prédire le Prix"):
         st.write("Lancer la prédiction avec les valeurs suivantes :")
         st.write(form_data)
-
+        input_data = pd.DataFrame([form_data])
         # Si le modèle s'attend à un tableau NumPy, convertissez input_data
         input_data_np = input_data.values  # Convertir en tableau NumPy si nécessaire
         # Préparation des données d'entrée pour la prédiction
